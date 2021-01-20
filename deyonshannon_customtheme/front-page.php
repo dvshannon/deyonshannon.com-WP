6d61f40287942575d9865138a20f607d62b40fd8
Deyon Shannon <deyonshannon@Deyons-Air-2.lan>
2021-01-19 20:14:13 -0500

    removed gitignore for front page

---
+++ deyonshannon_customtheme/front-page.php
@@ -0,0 +1,62 @@
+<?php
+get_header();
+?>
+
+<main class="container-flex px-5">
+    <div class="row">
+        <div class="col-sm-12 py-2">
+            <h2 class="project-heading">Projects</h2>
+        </div>
+        <div class="col-md-6 col-sm-12 py-4">
+            <div class="project">
+            <div class="project-image">
+                <img src="http://deyonshannon.local/wp-content/uploads/2020/10/pexels-negative-space-160107.jpg" alt="project">
+            </div>
+
+            <div class="project-description">
+                <h3>Project Name</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, fuga. Fugiat placeat possimus ipsa totam cum provident nulla, ad in amet delectus at mollitia! At ipsam neque repellat repellendus doloremque dolores doloribus, modi, tempora sunt sit, quia asperiores sed beatae.
+                </p>
+                <div class="project-buttons">
+                    <button href="#" class="btn-github">View Code
+                    <span aria-hidden class="btn-github__glitch">View Code</span>
+                    </button>
+                    
+                    <button href="#" class="btn-demo">Demo
+                    <span aria-hidden class="btn-demo__glitch">Demo</span>
+                    </button>
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-12 py-4">
+            <div class="project">
+                <div class="project-image">
+                    <img src="http://deyonshannon.local/wp-content/uploads/2020/10/pexels-negative-space-160107.jpg" alt="project">
+                </div>
+
+                <div class="project-description">
+                    <h3>Project Name</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, fuga. Fugiat placeat possimus ipsa totam cum provident nulla, ad in amet delectus at mollitia! At ipsam neque repellat repellendus doloremque dolores doloribus, modi, tempora sunt sit, quia asperiores sed beatae.
+                    </p>
+                    <div class="project-buttons">
+                    <button href="#" class="btn-github">View Code
+                    <span aria-hidden class="btn-github__glitch">View Code</span>
+                    </button>
+                    
+                    <button href="#" class="btn-demo">Demo
+                    <span aria-hidden class="btn-demo__glitch">Demo</span>
+                    </button>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</main>
+
+<?php
+get_footer();
+?>
